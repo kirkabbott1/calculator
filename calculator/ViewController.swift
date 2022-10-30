bc91b209d89b8da947205c0d9dc79ee4d23edf7c
Kirk Abbott <kirkabbott@Kirks-MBP.lan>
2022-10-30 19:35:08 -0400

    ios calculator ui initial

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Kirk Abbott on 10/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
